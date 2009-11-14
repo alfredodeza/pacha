2c1666bd150f4c2137de89420b10f4f10c1a176b
Alfredo Deza <alfredodeza@gmail.com>
2009-11-14 14:22:35 -0500

    added licenses

--- trunk/packages/vanilla.py
+++ trunk/packages/vanilla.py
@@ -1,9 +1,18 @@
-#!/usr/bin/env python
+# vanilla.py
 #
+# Copyright 2009 Alfredo Deza
 #
-# Vanilla installation of packages
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
 #
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranties of 
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+# PURPOSE.  See the GNU Affero General Public License for more details.
 #
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.#
 
 from subprocess import call
 
