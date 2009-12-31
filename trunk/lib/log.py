a4cb3deb4b94e6687f5327c440f3b57862f6a843
Alfredo Deza <alfredodeza@gmail.com>
2009-12-31 12:31:14 -0500

    log module handles permission denied

--- trunk/lib/log.py
+++ trunk/lib/log.py
@@ -7,21 +7,27 @@
 knowlingly."""
 
 import os
+import sys
 from time import strftime
 
-def log(module='pacha', type='INFO', line=''):
+def append(module='pacha', type='INFO', line=''):
     """Simple function to write to a log file"""
 
     log_file = '/var/log/pacha.log'
-    if os.path.isfile(log_file):
-        open_log = open(log_file, 'a')
-    else:
-        open_log = open(log_file, 'w')
+    try:
+        if os.path.isfile(log_file):
+            open_log = open(log_file, 'a')
+        else:
+            open_log = open(log_file, 'w')
 
-    timestamp = strftime('%b %d %H:%M:%S')
-    log_line = "%s %s %s %s" % (timestamp, type, module, line)
-    open_log.write(log_line+'\n')
-    open_log.close()
+        timestamp = strftime('%b %d %H:%M:%S')
+        log_line = "%s %s %s %s" % (timestamp, type, module, line)
+        open_log.write(log_line+'\n')
+        open_log.close()
+
+    except IOError:
+        sys.stderr.write("Permission denied to write log file")
+
 
 
 
