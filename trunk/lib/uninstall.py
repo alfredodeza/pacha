e8ceba3b31ce87e3524a421eeb8aa1b9c049f101
Alfredo Deza <alfredodeza@gmail.com>
2010-01-16 12:53:40 -0500

    added uninstall options to remove and stop the daemon

--- trunk/lib/uninstall.py
+++ trunk/lib/uninstall.py
@@ -7,15 +7,21 @@
 import os
 import shutil
 import log
+from subprocess import call
 
 def main():
     pacha_dir = '/opt/pacha'
     log.append(module='uninstall', line="removing pacha dir at /opt/pacha")
     
     try:
+        call('/etc/init.d/pacha stop', shell=True)
+        log.append(module='uninstall', line="stopped daemon")
+        os.remove('/etc/init.d/pacha')
+        log.append(module='uninstall', line="destroyed pacha daemon symlink")
         shutil.rmtree(pacha_dir)
         os.remove('/usr/bin/pacha')
         log.append(module='uninstall', line="destroyed symlink")
+        
     except OSError,e:
         log.append(module='uninstall', type='ERROR',
                 line=e)
