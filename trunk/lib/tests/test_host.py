ac19b1a2756f2dab037b074ef41a7c576eaead54
Alfredo Deza <alfredodeza@gmail.com>
2010-01-19 12:27:13 -0500

    adding host tests

--- trunk/lib/tests/test_host.py
+++ trunk/lib/tests/test_host.py
@@ -1,19 +1,31 @@
+import sys
+sys.path.append('../')
+import os
+import shutil
 import unittest
+import host
 
 class TestHost(unittest.TestCase):
-    def test___init__(self):
-        # host = Host(host, package)
-        assert False # TODO: implement your test here
-
+    
+    def setUp(self):
+        """init the host class"""
+        self.foo_host = host.Host(host='foo')
+    
     def test_create(self):
-        # host = Host(host, package)
-        # self.assertEqual(expected, host.create())
-        assert False # TODO: implement your test here
+        """Create a host folder"""
+        host_dir = '/opt/pacha/hosts/foo'
+        self.foo_host.create()
+        self.assertTrue(host_dir)
+
+    def tearDown(self):
+        """Remove the created folder"""
+        shutil.rmtree('/opt/pacha/hosts/foo')
 
 class TestHostname(unittest.TestCase):
     def test_hostname(self):
-        # self.assertEqual(expected, hostname())
-        assert False # TODO: implement your test here
+        """Get the right hostname"""
+        uname = os.uname()[1]
+        self.assertEquals(uname, host.hostname())
 
 if __name__ == '__main__':
     unittest.main()
