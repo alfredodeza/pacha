cbcf856ff0d5aca1386dbf45cd8c782cf5587427
Alfredo Deza <alfredodeza@gmail.com>
2010-02-01 16:47:47 -0500

    replace lib method

--- trunk/lib/upgrade.py
+++ trunk/lib/upgrade.py
@@ -16,23 +16,49 @@
 from subprocess import call, PIPE
 import log
 
-def get():
-    """Grab the latest version from code.google.com"""
-    # The stable release has to be always in the same spot:
-    stable = "https://pacha.googlecode.com/hg/branches/stable"
-    command = "hg clone %s /tmp/upgrade" % stable
-    log.append(module='upgrade', type='INFO', line="checking out pacha stable")
-    call(command, shell=True, stdout=PIPE)
 
-def replace_lib():
-    """Replaces all lib and test files"""
+class Replace(object):
+    """methods to perform different changes for the upgrade"""
 
-def replace_daemon():
-    """updates the daemon file"""
+    def __init__(self):
+        self.stable = "https://pacha.googlecode.com/hg/branches/stable"
+        self.tmp = "/tmp/upgrade"
 
-def replace_pacha():
-    """updates pacha.py"""
+    def get(self):
+        """Grab the latest version from code.google.com"""
+        command = "hg clone %s %s" % (self.stable, self.tmp)
+        log.append(module='upgrade', type='INFO', line="checking out pacha stable")
+        call(command, shell=True, stdout=PIPE)
+        log.append(module='upgrade', type='INFO', line="finished cloning for upgrade")
+
+    def lib(self):
+        """Replaces all lib and test files"""
+        # first move lib
+        lib_location = "/opt/pacha/lib"
+        lib_destination = "/tmp/lib"
+        lib_clone = "/tmp/upgrade/lib"
+        shutil.move(lib_location, lib_destination)
+        log.append(module='upgrade', type='INFO', line="moved lib to tmp")
+        # now we move the new lib into place
+        shutil.move(lib_clone, lib_destination)
+        log.append(module='upgrade', type='INFO', line="moved new lib into /opt/pacha")
+
+
+    def daemon(self):
+        """updates the daemon file"""
+        # move daemon
+        daemon_location = "/etc/init.d/pacha"
+        daemon_destination = "/tmp/pacha_daemon"
+        daemon_clone = "/tmp/upgrade/lib/daemon/pacha"
+
+
+    def pacha(self):
+        """updates pacha.py"""
 
 
 def main():
+    """does the upgrade step by step"""
+    # grab the latest
+    get()
+
 
