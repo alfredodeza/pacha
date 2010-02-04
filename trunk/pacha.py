b46a4b4d68f50410ae5747ba2fb188b18e6e6002
Alfredo Deza <alfredodeza@gmail.com>
2010-02-04 13:06:26 -0500

    added check for sudo privileges

--- trunk/pacha.py
+++ trunk/pacha.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
+import getpass
 import os
 import sys
 from optparse import OptionParser
@@ -41,56 +41,62 @@ def main():
     parser.add_option('--upgrade', action="store_true",
             help="Upgrades to a newer version")
 
-    options, arguments = parser.parse_args()
+    # Check for sudo privileges before annything else:
+    if getpass.getuser() != 'root':
+        sys.stderr.write(" * Pacha needs sudo privileges to run *\n")
+        sys.exit(1)
 
-    # Cleanest way to show the help menu if no options are given
-    if len(sys.argv) == 1:
-        parser.print_help()
+    else:
+        options, arguments = parser.parse_args()
 
-    if options.install:
-        install.main()
-        print "Pacha finished installing."
-        print "############################################################"
-        print """Remember to edit pacha.conf and run:
+        # Cleanest way to show the help menu if no options are given
+        if len(sys.argv) == 1:
+            parser.print_help()
+    
+        if options.install:
+            install.main()
+            print "Pacha finished installing."
+            print "############################################################"
+            print """Remember to edit pacha.conf and run:
 pacha --watch /opt/pacha/conf
 This will keep track of all host specific configurations that will be needed
 when rebuilding."""
 
-    if options.uninstall:
-        uninstall.main()
-        print "Pacha finished uninstalling."
+        if options.uninstall:
+            uninstall.main()
+            print "Pacha finished uninstalling."
+    
+        if options.add_host:
+            new = host.Host(host=options.add_host)
+            new.create()
+    
+        if options.watch:
+            # a hack to have ambiguous optparse behavior 
+            if len(sys.argv) is 2: #no path
+                path = os.getcwd()
+            if len(sys.argv) >=3: #with path
+                path = sys.argv[2]
+            mercurial = hg.Hg(path=path)
+            mercurial.hgrc()
+            # we do a first time clone:
+            mercurial.clone()
+            # add the path to .repos
+            repos = open('/opt/pacha/conf/.repos', 'a')
+            repos.write(path+'\n')
+            repos.close()
 
-    if options.add_host:
-        new = host.Host(host=options.add_host)
-        new.create()
+        if options.rebuild:
+            try:
+                run = rebuild.Rebuild()
+                run.retrieve_files()
+                run.install()
+                run.replace_manager()
 
-    if options.watch:
-        # a hack to have ambiguous optparse behavior 
-        if len(sys.argv) is 2: #no path
-            path = os.getcwd()
-        if len(sys.argv) >=3: #with path
-            path = sys.argv[2]
-        mercurial = hg.Hg(path=path)
-        mercurial.hgrc()
-        # we do a first time clone:
-        mercurial.clone()
-        # add the path to .repos
-        repos = open('/opt/pacha/conf/.repos', 'a')
-        repos.write(path+'\n')
-        repos.close()
+            except KeyboardInterrupt:
+                sys.exit(1)
 
-    if options.rebuild:
-        try:
-            run = rebuild.Rebuild()
-            run.retrieve_files()
-            run.install()
-            run.replace_manager()
-
-        except KeyboardInterrupt:
-            sys.exit(1)
-
-    if options.upgrade:
-        upgrade.main()
+        if options.upgrade:
+            upgrade.main()
 
 if __name__ == '__main__':
     main()
