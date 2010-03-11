8bcc25cd63b88bccb9984ca653fbdc4241b9749b
Alfredo Deza <alfredodeza@gmail.com>
2010-03-11 15:45:24 -0500

    added more tests to test_database.py

--- trunk/lib/tests/test_database.py
+++ trunk/lib/tests/test_database.py
@@ -24,12 +24,23 @@ class TestWorker(unittest.TestCase):
     def test_insert_path(self):
         """Do a simple insert of a path into db"""
         db = database.Worker(db='/tmp/pacha.db')
-        db.insert('/tmp/foo')
+        db.insert(path='/tmp/foo', type="dir")
         # create the connection again:
         db = database.Worker(db='/tmp/pacha.db')
         for i in db.get_repo('/tmp/foo'):
             actual = i[1]
         expected = u'/tmp/foo'
+        self.assertEqual(actual, expected)
+
+    def test_insert_type(self):
+        """Do a simple insert of a path and its type into db"""
+        db = database.Worker(db='/tmp/pacha.db')
+        db.insert(path='/tmp/foo', type="dir")
+        # create the connection again:
+        db = database.Worker(db='/tmp/pacha.db')
+        for i in db.get_repo('/tmp/foo'):
+            actual = i[3]
+        expected = u'dir'
         self.assertEqual(actual, expected)
 
     def test_remove(self):
