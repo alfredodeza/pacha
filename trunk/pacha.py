cfb1d0a0f45eb2588780a23702a120af24fe1b0f
Alfredo Deza <alfredodeza@gmail.com>
2010-03-13 13:32:14 -0500

    added changes to the main license

--- trunk/pacha.py
+++ trunk/pacha.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 #
-# Copyright 2009-2010 Alfredo Deza
 #
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3,
-# as published by the Free Software Foundation.
+# Copyright (c) 2009-2010 Alfredo Deza <alfredodeza [at] gmail [dot] com>
 #
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
-# PURPOSE.  See the GNU General Public License for more details.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 
 import getpass
 import os
@@ -101,7 +109,7 @@ when rebuilding."""
         if options.add_host:
             new = host.Host(host=options.add_host)
             new.create()
-    
+
         if options.watch:
             # a hack to have ambiguous optparse behavior 
             if len(sys.argv) is 2: #no path
