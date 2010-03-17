81bc4d6cdd3faf67ba1344312593fb4d512b6535
Alfredo Deza <alfredodeza@gmail.com>
2010-03-17 17:29:34 -0400

    left a lot of brackets...

--- trunk/lib/uninstall.py
+++ trunk/lib/uninstall.py
@@ -25,14 +25,14 @@ def main():
     try:
         if os.path.isfile('/etc/init.d/pacha'):
             call('/etc/init.d/pacha stop', shell=True)
-            print "Stopped daemon")
+            print "Stopped daemon"
             os.remove('/etc/init.d/pacha')
-        print "Destroyed pacha daemon symlink")
+        print "Destroyed pacha daemon symlink"
         if os.path.isdir(pacha_dir):
             shutil.rmtree(pacha_dir)
-            print "Removed /opt/pacha")
+            print "Removed /opt/pacha"
         os.remove('/usr/bin/pacha')
-        print "Destroyed symlink")
+        print "Destroyed symlink"
         
     except OSError,e:
         print e
