33cb19c1870bc7c536e5b0ff1601d74fc55743ca
Alfredo Deza <alfredodeza at gmail dot com>
2010-04-09 09:26:55 -0400

    list testing for confparser

--- tests/test_confparser.py
+++ tests/test_confparser.py
@@ -31,5 +31,26 @@ class TestParse(unittest.TestCase):
         actual = self.parse.one
         self.assertEqual(actual, expected)
 
+    def test_option_list(self):
+        """We should get a list from an option"""
+        expected = ['a']
+        actual = self.parse.two
+        self.assertEqual(actual, expected)
+
+    def test_option_string(self):
+        """We should get an item from a value list"""
+        expected = 'a'
+        actual = self.parse.two[0]
+        self.assertEqual(actual, expected)
+
+#    def test_option_string(self):
+#        """We should get a string from an option"""
+#        expected = 'True'
+#        actual = self.parse.one
+#        self.assertEqual(actual, expected)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
