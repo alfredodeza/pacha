8498662d40f23db19ccd8b82ed0e63ec472dbe8a
Alfredo Deza <alfredodeza at gmail dot com>
2010-04-09 10:53:16 -0400

    finished tests for confparser

--- tests/test_confparser.py
+++ tests/test_confparser.py
@@ -5,6 +5,34 @@ import unittest
 import os
 from lib import confparser
 
+class TestParseText(unittest.TestCase):
+
+    def setUp(self):
+        """Create a text file to read"""
+        txt = open('txt.conf', 'w')
+        txt.write('one\n')
+        txt.close()
+
+    def tearDown(self):
+        os.remove('txt.conf')
+
+    def test_text_append(self):
+        """Append a line to the end of the file"""
+        parse = confparser.Parse('txt.conf',
+                new_value = 'lastline')
+        parse.text_append()
+        txt = open('txt.conf')
+        actual = txt.readlines()[-1]
+        expected = 'lastline\n'
+        self.assertEqual(actual, expected)
+
+    def test_text_read_list(self):
+        """Get a list of all the values in the txt file"""
+        parse = confparser.Parse('txt.conf')
+        actual = parse.text_read()
+        expected = ['one']
+        self.assertEqual(actual, expected)
+
 class TestParse(unittest.TestCase):
 
     def setUp(self):
@@ -13,7 +41,7 @@ class TestParse(unittest.TestCase):
         config_file.write('one = True\n')
         config_file.write("""two = ['a']\n""")
         config_file.write('three = {"number":"three"}\n')
-        config_file.write('# a comment that should not be readi\n')
+        config_file.write('#comment =\n')
         config_file.close()
         self.parse = confparser.Parse('test.conf')
         self.parse.options()
@@ -37,19 +65,28 @@ class TestParse(unittest.TestCase):
         actual = self.parse.two
         self.assertEqual(actual, expected)
 
-    def test_option_string(self):
+    def test_option_list_value(self):
         """We should get an item from a value list"""
         expected = 'a'
         actual = self.parse.two[0]
         self.assertEqual(actual, expected)
 
-#    def test_option_string(self):
-#        """We should get a string from an option"""
-#        expected = 'True'
-#        actual = self.parse.one
-#        self.assertEqual(actual, expected)
+    def test_option_dictionary(self):
+        """We should get a dictionary from an option"""
+        expected = {"number":"three"}
+        actual = self.parse.three
+        self.assertEqual(actual, expected)
 
+    def test_option_dictionary_value(self):
+        """We should get an item from a value dictionary"""
+        expected = "three"
+        actual = self.parse.three['number']
+        self.assertEqual(actual, expected)
 
+    def test_option_comment(self):
+        """A comment should not be evaluated"""
+        actual = hasattr(self.parse, 'comment')
+        self.assertFalse(actual)
 
 
 if __name__ == '__main__':
