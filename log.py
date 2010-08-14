743e40171deecd30d92ae1301164b6870cd54f8d
Alfredo Deza <alfredodeza@gmail.com>
2010-08-14 12:59:11 -0400

    removed log.py in wrong spot

--- log.py
+++
@@ -1,8 +0,0 @@
-from logging import getLogger
-
-## Set logger objects for all modules
-daemon          = getLogger('pacha.daemon')
-database        = getLogger('pacha.database')
-hg              = getLogger('pacha.hg')
-host            = getLogger('pacha.host')
-permissions     = getLogger('pacha.permissions')
