ae754a299a6d5ad6a36c8687666507492c99b519
Alfredo Deza <alfredodeza@gmail.com>
2010-08-14 13:04:47 -0400

    fixing dependencies testing

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,20 +1,40 @@
-import sys
-if '../' not in sys.path:
-    sys.path.append('../')
 import unittest
-import os
 
 class TestDependencies(unittest.TestCase):
 
-    def test_ssh_bin(self):
-        """Is ssh in /usr/bin"""
-        ssh = os.path.exists('/usr/bin/ssh')
-        self.assertTrue(ssh)
 
-    def test_hg_bin(self):
-        """Is Mercurial in /usr/bin"""
-        hg = os.path.exists('/usr/bin/hg')
-        self.assertTrue(hg)
+    def test_hg(self):
+        try:
+            import mercurial 
+            imported = True
+        except:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_hg_commands(self):
+        try:
+            from mercurial import commands
+            imported = True
+        except:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_hg_ui(self):
+        try:
+            from mercurial import ui
+            imported = True
+        except:
+            imported = False
+        self.assertTrue(imported)
+
+    def test_hg_hg(self):
+        try:
+            from mercurial import hg
+            imported = True
+        except:
+            imported = False
+        self.assertTrue(imported)
+
 
 if __name__ == '__main__':
     unittest.main()
