eebd73df70af4a1a36c115f6c9ab8ef63ac5c063
Alfredo Deza <alfredodeza@gmail.com>
2010-08-17 21:12:32 -0400

    added cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,33 +1,34 @@
-#import unittest
-#from subprocess import Popen, PIPE
-#
-#
-#
-#
-#class TestCommandLine(unittest.TestCase):
-#
-#    def test_no_arguments(self):
-#        command = "pacha"
-#        run = Popen(command, shell=True, stdout=PIPE)
-#        actual = run.stdout.readlines()
-#        warning_msg = 'Warning! You have not set a configuration file for Pacha.\n'
-#        if warning_msg in actual:
-#            warning = True
-#        else:
-#            warning = False
-#        self.assertTrue(warning)
-#
-#    def test_no_config_warning(self):
-#        command = "pacha --watch"
-#        run = Popen(command, shell=True, stdout=PIPE)
-#        actual = run.stdout.readlines()
-#        warning_msg = 'Warning! You have not set a configuration file for Pacha.\n'
-#        if warning_msg in actual:
-#            warning = True
-#        else:
-#            warning = False
-#        self.assertTrue(warning)
-#
-#
-#if __name__ == '__main__':
-#    unittest.main()
+import unittest2
+from subprocess import Popen, PIPE
+
+
+
+
+class TestCommandLine(unittest2.TestCase):
+
+    def test_no_arguments(self):
+        command = "pacha"
+        run = Popen(command, shell=True, stdout=PIPE)
+        actual = run.stdout.readlines()
+        warning_msg =' |                 ** WARNING **                      |\n' 
+        print actual
+        if warning_msg in actual:
+            warning = True
+        else:
+            warning = False
+        self.assertTrue(warning)
+
+    def test_no_config_warning(self):
+        command = "pacha --watch"
+        run = Popen(command, shell=True, stdout=PIPE)
+        actual = run.stdout.readlines()
+        warning_msg =' |                 ** WARNING **                      |\n' 
+        if warning_msg in actual:
+            warning = True
+        else:
+            warning = False
+        self.assertTrue(warning)
+
+
+if __name__ == '__main__':
+    unittest2.main()
