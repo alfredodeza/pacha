e9660a080b5952520aec55c0187c921b88612748
Alfredo Deza <alfredodeza@gmail.com>
2010-09-10 19:30:57 -0400

    adding tests for util and fixed some tests that broke when rewriting the daemon

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -4,33 +4,15 @@ import sys
 
 from pacha import daemon
 from mock import MockSys
-#class MockSys(object):
-#    """Can grab messages sent to stdout or stderr"""
-#    def __init__(self):
-#        self.message = ""
-#
-#    def write(self, string):
-#        self.message = string 
-#        pass
-#
 
 class TestWatcher(unittest.TestCase):
 
     def test_init(self):
         """Should get a normpath from a path"""
-        path = "/path/to/a/single/file.txt/"
+        path = "/tmp/"
         watch = daemon.Watcher(path)
         actual = watch.path 
-        expected = "/path/to/a/single/file.txt"
-        self.assertEqual(actual, expected) 
-
-class TestRunners(unittest.TestCase):
-
-    def test_init_chdir(self):
-        """Should change dir"""
-        init = daemon.Runners('/')
-        actual = os.getcwd()
-        expected = '/'
+        expected = "/tmp"
         self.assertEqual(actual, expected) 
 
 class TestFrecuency(unittest.TestCase):
@@ -57,20 +39,6 @@ class TestFrecuency(unittest.TestCase):
         "No matter what we send we get 60 secs back"
         actual = daemon.frecuency({})
         expected = 60 
-        self.assertEqual(actual, expected) 
-
-class RunCommand(unittest.TestCase):
-
-    def test_run_command_stdout(self):
-        sys.stderr = MockSys()
-        actual = daemon.run_command(std="stdout", cmd="""echo "foo" """)
-        expected = ['foo\n']
-        self.assertEqual(actual, expected) 
-
-    def test_run_command_stderr(self):
-        sys.stderr = MockSys()
-        actual = daemon.run_command(std="stderr", cmd=""" echo "error message" 1>&2 """)
-        expected = ['error message\n']
         self.assertEqual(actual, expected) 
 
 if __name__ == '__main__':
