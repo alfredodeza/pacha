5a72e173a2904a7e7fefa13ee4119dbbdacb3d66
Alfredo Deza <alfredodeza@gmail.com>
2010-10-09 14:32:52 -0400

    minor update to setup.py for a better long description

--- setup.py
+++ setup.py
@@ -32,6 +32,32 @@ Pacha's approach is to easily deploy an instance and capture any changes
 via a version control, giving the System Administrator the ability 
 to rollback and safe guard valid, working configurations and rebuild a 
 host from those same configurations.
+
+Don't learn another language
+------------------------------
+Other configuration frameworks tell you this, but are written in a language like Ruby and require you to extend,modify or adapt this writing Ruby. We don't! It doesn't matter that Pacha is written in Python. You do not need to know Python to make any changes to the software you want to deploy.
+
+Easy learn curve
+-----------------------
+One main configuration file to manage the package to be installed and one directory where you can place any number of executable shell scripts to extend and modify your deployment according to your needs.
+
+Versioned Configurations
+----------------------------
+Keep track of any changes done to a configuration file and rollback easily. We use Mercurial as our back-end but you do not need to manage it directly. Pacha can push changes immediately to any number of clients and get instant feedback.
+
+Light
+---------
+Deploy Pacha as a client, server, or standalone version. Either way, you get an extremely light, low memory tool that can communicate with your configuration files almost instantly.
+
+Extend and Conquer
+------------------------
+Forget about the "but this tool doesn't do this". We provide a full hook environment to plug in directly into Pacha so you can extend whatever need you have for your deployment.
+
+Security
+-----------
+Transferring system configuration files, even across a trusted private network is a security risk. Pacha tackles this problem using SSH as the transport protocol. Although this may add a slight complexity to the framework, we encourage security and think this is the best shot for dealing with critical data related to your systems.
+
+
 """,
    classifiers=[
         'Development Status :: 4 - Beta',
@@ -45,7 +71,7 @@ host from those same configurations.
 
     license = "MIT",
     keywords = "systems configuration management engine hg mercurial",
-    url = "http://code.google.com/p/pacha",   
+    url = "http://pacha.cafepais.com",   
 
 )
 
