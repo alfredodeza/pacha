91d27feb029ae0bb5f136d14d17c2bfab0ac379c
Alfredo Deza <alfredodeza@gmail.com>
2010-10-24 13:00:52 -0400

    adds the directory for all the deployment related actions in pacha

---
+++ pacha/spread/seed.py
@@ -0,0 +1,10 @@
+"""
+Builds the information necessary to do a new deployment.
+
+Specifically, we need:
+
+ * List of directories to create.
+ * A unique UUID for the deployment
+ * List of absolute paths for top level directories to replace
+
+"""
