cbc336c6b24e4d8e6b608fcd856dd00c34bb88b6
Alfredo Deza <alfredodeza@gmail.com>
2010-10-24 14:07:58 -0400

    adds docstrings with the protocol to make the sharing happen

--- pacha/spread/seed.py
+++ pacha/spread/seed.py
@@ -6,5 +6,5 @@ Specifically, we need:
  * List of directories to create.
  * A unique UUID for the deployment
  * List of absolute paths for top level directories to replace
-
+ * Should be able to push the seeding file to N hosts. (it should know abouut those hosts)
 """
